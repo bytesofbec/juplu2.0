88cc758c7b80f9a043032e1502cbc25db70514de
Rebecca Mathews <bytesofbec@gmail.com>
2024-07-12 16:27:14 +0530

    build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
+pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
